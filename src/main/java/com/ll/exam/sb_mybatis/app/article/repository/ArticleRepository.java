0db305fbaba2b8d3243172a4acc4fd67db58f0e7
Aruming <0430kar@naver.com>
2022-09-23 15:27:03 +0900

    게시물 등록

--- src/main/java/com/ll/exam/sb_mybatis/app/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/exam/sb_mybatis/app/article/repository/ArticleRepository.java
@@ -1,6 +1,7 @@
 package com.ll.exam.sb_mybatis.app.article.repository;
 
 import com.ll.exam.sb_mybatis.app.article.dto.Article;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -16,4 +17,15 @@ public interface ArticleRepository {
             </script>
             """)
     List<Article> getArticles();
+
+    @Insert("""
+            <script>
+            INSERT INTO article
+            SET createDate = NOW(),
+            modifyDate = NOW(),
+            subject = #{subject},
+            content = #{content}
+            </script>
+            """)
+    void write(String subject, String content);
 }
