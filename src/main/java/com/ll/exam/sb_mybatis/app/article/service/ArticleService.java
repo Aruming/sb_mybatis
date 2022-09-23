3c0b7d19a544e2572411f869e79f64971e578c72
Aruming <0430kar@naver.com>
2022-09-23 15:22:45 +0900

    게시물 리스트 조회

---
+++ src/main/java/com/ll/exam/sb_mybatis/app/article/service/ArticleService.java
@@ -0,0 +1,18 @@
+package com.ll.exam.sb_mybatis.app.article.service;
+
+import com.ll.exam.sb_mybatis.app.article.dto.Article;
+import com.ll.exam.sb_mybatis.app.article.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    public List<Article> getArticles() {
+        return articleRepository.getArticles();
+    }
+}
