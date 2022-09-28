ffd0eebae02eeec187d8ab0e61ed14b502387feb
Aruming <0430kar@naver.com>
2022-09-29 00:37:10 +0900

    session ion 스코프

--- src/main/java/com/ll/exam/sb_mybatis/app/base/rq/Rq.java
+++ src/main/java/com/ll/exam/sb_mybatis/app/base/rq/Rq.java
@@ -3,8 +3,10 @@ package com.ll.exam.sb_mybatis.app.base.rq;
 import lombok.Getter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Component
+@SessionScope
 public class Rq {
     @Getter
     private int count;
